1581762850d2b9b8463a5187cadf5cb88d7e253b
daniya <daniyamughal234@gmail.com>
2024-01-13 19:03:22 +0500

    done

---
+++ 5th.js
@@ -0,0 +1,34 @@
+let age = 10;
+
+if(age >= 20){
+    console.log("you can cast your vote");
+}
+
+if(age >= 20){
+    console.log("you can not cast your vote");
+}
+
+
+if(age >=20){
+    console.log("you can cast your vote");
+}
+
+else{
+    console.log("you can not cast your vote");
+
+}
+
+
+let mode = prompt(`enter your mood
+1) dark
+2) light`);
+
+let color;
+
+if (mode === "dark"){
+    color = "black";
+}
+if(mode === "light");{
+   color = "white"
+}   
+
---
+++ 6th.js
@@ -0,0 +1,33 @@
+   
+
+
+   for(let i=0; i<=10;i++){
+    console.log("PJS" , i) ;
+   }
+     
+   for(let i=1; i<=10;i++){
+    console.log("2 x ", i,"=" ,2*i)
+   }
+   
+let num =70;
+
+// while loop
+while(num<=75){
+    console.log("this is while loop" , num);
+    num++;
+}
+// string enterpolation
+// first method~
+let myvalue = prompt("enter your name");
+for(let y=1; y<=10; y++){
+    console.log(`${myvalue} x ${y} = ${myvalue*y}`)
+}
+
+// second method 
+let value = prompt("enter your name");
+for(let y=1; y<=10; y++){
+    console.log(value ,"x",y ,value*y)
+} 
+
+
+
